073a2055da833003069cd19e1e2a424fc1b9df2e
Akki <chauhanakshit61@gmail.com>
2025-03-17 09:44:04 +0530

    [Akshit] ADD: Creating a vehicle transport management system

---
+++ VehicleTransportSystem.java
@@ -0,0 +1,77 @@
+class Vehicle {
+    protected int maxSpeed;
+    protected String fuelType;
+
+    public Vehicle(int maxSpeed, String fuelType) {
+        this.maxSpeed = maxSpeed;
+        this.fuelType = fuelType;
+    }
+
+    public void displayInfo() {
+        System.out.println("Max Speed: " + maxSpeed + " km/h, Fuel Type: " + fuelType);
+    }
+}
+
+// Car subclass with seatCapacity attribute
+class Car extends Vehicle {
+    private int seatCapacity;
+
+    public Car(int maxSpeed, String fuelType, int seatCapacity) {
+        super(maxSpeed, fuelType);
+        this.seatCapacity = seatCapacity;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Seat Capacity: " + seatCapacity);
+    }
+}
+
+// Truck subclass with loadCapacity attribute
+class Truck extends Vehicle {
+    private int loadCapacity;
+
+    public Truck(int maxSpeed, String fuelType, int loadCapacity) {
+        super(maxSpeed, fuelType);
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Load Capacity: " + loadCapacity + " tons");
+    }
+}
+
+// Motorcycle subclass with hasSidecar attribute
+class Motorcycle extends Vehicle {
+    private boolean hasSidecar;
+
+    public Motorcycle(int maxSpeed, String fuelType, boolean hasSidecar) {
+        super(maxSpeed, fuelType);
+        this.hasSidecar = hasSidecar;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Has Sidecar: " + (hasSidecar ? "Yes" : "No"));
+    }
+}
+
+// Main class demonstrating polymorphism
+public class VehicleTransportSystem {
+    public static void main(String[] args) {
+        Vehicle[] vehicles = {
+                new Car(200, "Petrol", 5),
+                new Truck(120, "Diesel", 15),
+                new Motorcycle(180, "Petrol", false)
+        };
+
+        for (Vehicle vehicle : vehicles) {
+            vehicle.displayInfo();
+            System.out.println();
+        }
+    }
+}
